735496532c7bddd06eef1ec30bea5596334cd77c
Brittney Kistner <bkistner01@gmail.com>
2014-06-11 09:51:00 -0700

    Completed War methods.  Wrote War initialize tests.

--- WAR/spec/war_spec.rb
+++ WAR/spec/war_spec.rb
@@ -3,3 +3,82 @@ require 'rspec'
 require 'pry-debugger'
 require_relative '../war.rb'
 
+describe Card do
+  it 'checks the value, suit and rank of a card' do
+    card = Card.new(11, :diamonds)
+    expect(card.value).to eq(11)
+    expect(card.suit).to eq(:diamonds)
+    expect(card.rank). to eq(:J)
+  end
+end
+
+describe Deck do
+  it 'initializes an empty deck' do
+    new_deck = Deck.new
+    expect(new_deck.deck).to eq([])
+  end
+
+  it 'checks the suits in the deck' do
+    expect(Deck.suits).to eq([:spades, :diamonds, :clubs, :hearts])
+  end
+
+  it 'checks the values in the deck' do
+    expect(Deck.values).to eq([2,3,4,5,6,7,8,9,10,11,12,13,14])
+  end
+
+  it 'adds 52 cards to the deck' do
+    deck1 = Deck.new
+    deck1.create_52_card_deck
+    expect(deck1.deck.length).to eq(52)
+
+    deck1.create_52_card_deck
+    expect(deck1.deck.length).to eq(104)
+  end
+
+  it 'removes a card from the deck' do
+    deck1 = Deck.new
+    deck1.create_52_card_deck
+    expect(deck1.current_index).to eq(0)
+    deck1.remove_card
+    expect(deck1.current_index).to eq(1)
+  end
+
+  it 'checks if a card object is returned when remove card is called' do
+    deck1 = Deck.new
+    deck1.create_52_card_deck
+    expect(deck1.remove_card).to be_a(Card)
+  end
+
+  it 'checks if the current index has switched to the placeholder deck and current index is 0' do
+    deck1 = Deck.new
+    deck1.create_52_card_deck
+    expect(deck1.current_index).to eq(0)
+    52.times do
+      deck1.remove_card
+    end
+    expect(deck1.current_index).to eq(0)
+  end
+
+  it 'shuffles cards' do
+    deck1 = Deck.new
+    deck1.create_52_card_deck
+    deck1.shuffle
+    expect(deck1.deck.size).to eq(52)
+  end
+end
+
+describe War do
+  describe "initializes"
+    it "War with two players and a full main deck" do
+      new_game = War.new("sky", "fawn")
+      expect(new_game.player1).to eq("sky")
+      expect(new_game.player2).to eq("fawn")
+    end
+  describe 'initializes'
+    it "with 26 cards in each player's deck" do
+      expect(player1.hand.count).to eq(26)
+      expect(player2.hand.count).to eq(26)
+    end
+  end
+end
+
--- WAR/war.rb
+++ WAR/war.rb
@@ -3,62 +3,158 @@ class Card
   # Rank is the rank of the card, 2-10, J, Q, K, A
   # Value is the numeric value of the card, so J = 11, A = 14
   # Suit is the suit of the card, Spades, Diamonds, Clubs or Hearts
-  def initialize(rank, value, suit)
-    @rank = rank
+
+  attr_reader :value, :suit, :rank
+  def initialize(value, suit) #consider adding the rank back in
+    # @rank = rank
     @value = value
     @suit = suit
+    @rank = make_rank(value)
+  end
+
+  def make_rank(value)
+  case value
+    when 11
+      return :J
+    when 12
+      return :Q
+    when 13
+      return :K
+    when 14
+      return :A
+    else
+      return nil
+    end
   end
 end
 
 # TODO: You will need to complete the methods in this class
 class Deck
-  attr_accessor :deck
+  def self.suits
+    [:spades, :diamonds, :clubs, :hearts]
+  end
+
+  def self.values
+    [2,3,4,5,6,7,8,9,10,11,12,13,14]
+  end
+
+  attr_accessor :deck, :current_index
+  attr_reader :placeholder
+
   def initialize
-    @deck = nil # Determine the best way to hold the cards
+    @deck = [] # Determine the best way to hold the cards
+    @ph = [] #ph = placeholder array
+    @current_index = 0
   end
 
   # Given a card, insert it on the bottom your deck
   def add_card(card)
-
+    @ph << card
   end
 
+#REVIEW
   # Mix around the order of the cards in your deck
   def shuffle # You can't use .shuffle!
-
+    @deck.size.times do |i|
+      j = rand(deck.size)
+      @deck[i], @deck[j] = @deck[j], @deck[i]
+    end
   end
 
   # Remove the top card from your deck and return it
-  def deal_card
+  def remove_card
+    card = @deck[@current_index]  #the first time around, this would be @deck[0]
+    @deck[@current_index] = nil #now the first element is nil
+    @current_index += 1 #increment the index by 1
+    check_array
+    return card
+  end
 
+  def swap_decks
+    @deck = @ph
+    @current_index = 0
+    @ph = []
   end
 
   # Reset this deck with 52 cards
   def create_52_card_deck
-
+    Deck.suits.each do |suit|
+      Deck.values.each do |value|
+        @deck << Card.new(value, suit)
+      end
+    end
   end
 
+
+  def check_array
+    if @deck[@current_index] == nil
+      Deck.swap_decks
+    end
+  end
+
+  def empty?
+    @deck.count {|x| x!=nil} == 0
+  end
 end
 
-# You may or may not need to alter this class
 class Player
   def initialize(name)
     @name = name
     @hand = Deck.new
   end
+  def give_card(card)
+    @hand.add_card
+  end
+  def has_cards?
+    if @hand.empty? == true
+      false
+    else
+      true
+    end
+  end
+
+  def play_card
+    @deck.remove_card
+  end
 end
 
 
 class War
+
+  attr_accessor :player1, :player2, :main_deck
+
   def initialize(player1, player2)
-    @deck = Deck.new
+    @main_deck = Deck.new
+    @main_deck.create_52_card_deck
+    @main_deck.shuffle
+
     @player1 = Player.new(player1)
     @player2 = Player.new(player2)
-    # You will need to shuffle and pass out the cards to each player
+
+    pass_cards
   end
 
-  # You will need to play the entire game in this method using the WarAPI
+  def pass_cards
+    while !@main_deck.empty?
+      card1 = @main.deck.remove_card
+      @player1.give_card(card1)
+      card2 = @main.deck.remove_card
+      @player2.give_card(card2)
+    end
+    return "ready to play"
+  end
+
   def play_game
-    # WarAPI.play_turn()
+    while @player1.has_cards? && @player2.has_cards?
+      card1 = @player1.play_card
+      card2 = @player2.play_card
+      WarAPI.play_turn(@player1, card1, @player2, card2)
+    end
+    if @player1.hand.empty?
+      return @player2
+    else
+      return @player1
+    end
   end
 end
 
@@ -69,7 +165,9 @@ class WarAPI
   def self.play_turn(player1, card1, player2, card2)
     if card1.value > card2.value
       {player1 => [card1, card2], player2 => []}
-    elsif card2.value > card1.value || Rand(100).even?
+    elsif card2.value > card1.value
+      {player1 => [], player2 => [card2, card1]}
+    elsif Rand(100).even?
       {player1 => [], player2 => [card2, card1]}
     else
       {player1 => [card1, card2], player2 => []}
